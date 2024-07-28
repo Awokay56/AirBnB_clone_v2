8c5e24e8b698a3be6438b01dc7a25f522791de45
Your Name <you@example.com>
2024-07-28 06:46:19 -0700

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+starts a flask web application
+"""
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    This is the home route handler.
+    """
+    return 'Hello HBNB'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+
+    """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
